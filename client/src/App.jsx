3c5a52611e2f5fa8ecfb6c9a5f3e325b25746172
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-08-12 11:25:05 +0600

    Client side initialized is done

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <div className="text-5xl uppercase text-center underline">App</div>
+      <button className="btn btn-primary">click me</button>
+    </div>
+  );
+}
--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const chats = require("./data/Data");
 dotenv.config();
 const app = express();
 
 
+// middleware
+app.use(express.json())
+
 // get besic server
 app.get('/', (req, res) => {
     res.send('This is my practice server...')
+})
+
+// get besic server
+app.get('/api/users', (req, res) => {
+    res.send(chats)
 })
 
 const port = process.env.PORT || 5000;
